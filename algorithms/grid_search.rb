4ced1bfd583b717672be0efff785ae98a96ed317
Anthony So <sokam@pdx.edu>
2015-09-21 14:51:01 -0700

    working on using regex for grid search

--- algorithms/grid_search.rb
+++ algorithms/grid_search.rb
@@ -5,8 +5,17 @@ COL = 1
 FIELD = 0
 MATCH = 1
 
+class String
+  def indices e
+    start, result = -1, []
+    result << start while start = (self.index e, start + 1)
+    result
+  end
+end
+
 num = gets.chomp.to_i
-size = Array.new(2) { Array.new(2, 0) }
+size = Array.new(2) { Array.new(2, 0) 
+match = Array.new(2)}
 grid = Array.new(2)
 
 num.times do 
@@ -14,10 +23,26 @@ num.times do
 		size[t] = gets.strip.split(" ").map(&:to_i)
 		grid[t] = Array.new(size[t][ROW]) { Array.new(size[t][COL], 0) }
 		(0...size[t][ROW]).each do |r|
-			grid[t][r] = gets.strip.split("").map(&:to_i)
-		end			
+			grid[t][r] = gets.chomp.to_s
+#			grid[t][r] = gets.strip.split("").map(&:to_i)
+		end	
 	end
 
+	string = grid[Field].join
+	
+
+	max = size[FIELD][ROW] - size[MATCH][ROW]
+	match = Array.new(size[MATCH][ROW])
+
+	(0..max[ROW]).each do |r|
+		(0..size[MATCH][ROW]).each do |m_r|
+			match[m_r] = grid[FIELD][r].indices /#{grid[MATCH][m_r]}/
+		end
+	end
+
+
+
+=begin
 	find = false
 	max = size.transpose.map { |x| x.reduce(:-) }
 	(0..max[ROW]).each do |r|
@@ -40,11 +65,11 @@ num.times do
 	else
 		puts "NO"
 	end
-=begin
+=end
+
 	(0..1).each do |t|
 		(0...size[t][ROW]).each do |r|
-			puts grid[t][r].join
+			puts grid[t][r]
 		end
 	end
-=end
 end
