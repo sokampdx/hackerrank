6403495fcfb55d96dd4297bb1890e36a609d53f6
sokampdx <sokam@pdx.edu>
2015-09-23 00:33:24 -0700

    working on shift table

--- algorithms/grid_search.rb
+++ algorithms/grid_search.rb
@@ -5,7 +5,27 @@ COL = 1
 FIELD = 0
 MATCH = 1
 
-def find_match(grid, size)
+def create_shift_table
+	table = Array.new(2)
+	table[ROW] = Array.new(10, size[MATCH][ROW])
+	table[COL] = Array.new(10, size[MATCH][COL])
+
+	
+
+end
+
+def find_match
+	# Horspool's 2D
+
+	$shift = create_shift_table
+	start = Array.new(2)
+	start[ROW] = 
+	start = size[MATCH].map { |x| x-1 }
+
+	
+	
+
+
 	# brute_force
 	find = false
 	max = size.transpose.map { |x| x.reduce(:-) }
@@ -32,17 +52,17 @@ def find_match(grid, size)
 end
 
 num = gets.chomp.to_i
-size = Array.new(2) { Array.new(2, 0) }
-grid = Array.new(2)
+$size = Array.new(2) { Array.new(2, 0) }
+$grid = Array.new(2)
+$shift = Array.new(2)
 
 num.times do 
 	(0..1).each do |t|
-		size[t] = gets.strip.split(" ").map(&:to_i)
-		grid[t] = Array.new(size[t][ROW]) { Array.new(size[t][COL], 0) }
-		(0...size[t][ROW]).each do |r|
-			grid[t][r] = gets.strip.split("").map(&:to_i)
+		$size[t] = gets.strip.split(" ").map(&:to_i)
+		$grid[t] = Array.new($size[t][ROW]) { Array.new($size[t][COL], 0) }
+		(0...$size[t][ROW]).each do |r|
+			$grid[t][r] = gets.strip.split("").map(&:to_i)
 		end			
 	end
-
-	puts find_match(grid, size)
+	puts find_match
 end
