2f204c11c881709171a8fa73e39ed290b17739bd
sokampdx <sokampdx@users.noreply.github.com>
2025-05-21 21:47:13 -0700

    interview prep completed: sale_by_match

---
+++ interview_prep/sale_by_match.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    color = {}
+    pair = 0
+    for i in range(n):
+        if ar[i] in color and color[ar[i]] == 1:
+            pair += 1
+            color[ar[i]] = 0
+        else:
+            color[ar[i]] = 1
+
+    return pair
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
