992c40297a402fb7142d2d99f46434d0efee751d
sokampdx <sokampdx@users.noreply.github.com>
2025-05-21 21:54:58 -0700

    interview prep completed: counting valley

---
+++ interview_prep/counting_valley.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    altitude = 0
+    count = 0
+    for s in range(steps):
+        if path[s] == 'D':
+            if altitude == 0:
+                count += 1
+            altitude -= 1
+        else:
+            altitude += 1
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
