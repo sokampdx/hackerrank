647d24ea6fac29c0901869be336bdef0e70160a1
sokam-maclinux <sokam-maclinux@users.noreply.github.com>
2025-09-06 17:36:33 -0700

    interview prep complete: alternating character

---
+++ interview_prep/alternating_characters.py
@@ -0,0 +1,40 @@
+[200~#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    prev = None
+    count = 0
+    for i, c in enumerate(s):
+        if i == 0:
+             prev = c
+        elif c == prev:
+            count += 1
+        else:
+            prev = c
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
